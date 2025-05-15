5176ceeb2c590158eb79f59837461e6f08b30c16
sebastiangome <sea.gomez@duocuc.cl>
2025-05-15 19:49:43 -0400

    Delete reserva/reserva directory

--- reserva/reserva/src/main/java/com/example/reserva/repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.reserva.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.reserva.model.Reserva;
-
-public interface ReservaRepository extends JpaRepository<Reserva,Long>{
-
-}
