46f67fddd9dbda523980fab44f846f1b6e190407
sebastiangome <sea.gomez@duocuc.cl>
2025-05-21 19:46:15 -0400

    solicitudes
    gestion de solicitud

---
+++ solicitudes/solicitudes/src/main/java/com/example/solicitudes/model/Solicitud.java
@@ -0,0 +1,33 @@
+package com.example.solicitudes.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name = "Solicitud")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Solicitud {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Asegúrate de definir un identificador
+    
+    @Column(nullable = false)
+    private Date fecha_solicitud; // Fecha de la solicitud
+
+    @Column(nullable = true) // Puede ser nulo
+    private String comentario; // Comentarios adicionales
+    
+    @Column(nullable = false)
+    private String total; // Total de la solicitud
+}
